8fc202179aa3fc081ef5af30f414b770f65883fb
Rajesh Poraiyan <rajeshporaiyan@rajeshs-mbp.lan>
2021-01-04 21:50:25 -0500

    initial commit to explore the execution context idea

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+pub trait ExecutionContext {
+    fn name(&self) -> &str {
+        "DefaultExecutor"
+    }
+    fn log(&self, message: &str) {
+        println!("{}-{}", self.name(), message)
+    }
+}
+pub trait BusinessLogic {
+    fn execute(&self, input: &str, execution_context: &impl ExecutionContext) -> String;
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::{BusinessLogic, ExecutionContext};
+
+    struct GreeterFunction;
+    impl BusinessLogic for GreeterFunction {
+        fn execute(&self, input: &str, execution_context: &impl ExecutionContext) -> String {
+            execution_context.log(format!("received a request {}", input).as_str());
+            format!("Hello, {}", input)
+        }
+    }
+
+    struct StandaloneContext;
+    impl ExecutionContext for StandaloneContext {
+        fn name(&self) -> &str {
+            "StandaloneExecutor"
+        }
+    }
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_works_for_default_execution_context() {
+        let greeter = GreeterFunction{};
+        let context = StandaloneContext {};
+        assert_eq!("Hello, Rajesh", greeter.execute("Rajesh", &context));
     }
 }
